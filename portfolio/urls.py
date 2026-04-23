cd7f3cf457a12856d52c7d39c9391635555ac1a5
Nidhi Mogatlal <a22405789@alunos.ulht.pt>
2026-04-23 17:54:28 +0000

    django ficha7

---
+++ portfolio/urls.py
@@ -0,0 +1,33 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.licenciaturas_view),
+    path('licenciaturas/', views.licenciaturas_view, name='licenciaturas'),
+    path('licenciatura/<int:id>/', views.licenciatura_view, name='licenciatura'),
+
+    path('docentes/', views.docentes_view, name='docentes'),
+    path('docente/<int:id>/', views.docente_view, name='docente'),
+
+    path('ucs/', views.ucs_view, name='ucs'),
+    path('uc/<int:id>/', views.uc_view, name='uc'),
+
+    path('tecnologias/', views.tecnologias_view, name='tecnologias'),
+    path('tecnologia/<int:id>/', views.tecnologia_view, name='tecnologia'),
+
+    path('competencias/', views.competencias_view, name='competencias'),
+    path('competencia/<int:id>/', views.competencia_view, name='competencia'),
+    
+    path('tfcs/', views.tfcs_view, name='tfcs'),
+    path('tfc/<int:id>/', views.tfc_view, name='tfc'),
+
+    path('projetos/', views.projetos_view, name='projetos'),
+    path('projeto/<int:id>/', views.projeto_view, name='projeto'),
+
+    path('formacoes/', views.formacoes_view, name='formacoes'),
+
+    path('makingofs/', views.makingofs_view, name='makingofs'),
+
+    path('eventos/', views.eventos_view, name='eventos'),
+    path('evento/<int:id>/', views.evento_view, name='evento'),
+]
--- portfolio/views.py
+++ portfolio/views.py
@@ -5,3 +5,81 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Home do projeto 🚀")
+
+from django.shortcuts import render, get_object_or_404
+from .models import *
+
+
+def licenciaturas_view(request):
+    licenciaturas = Licenciatura.objects.all()
+    return render(request, 'portfolio/licenciaturas.html', {'licenciaturas': licenciaturas})
+
+def licenciatura_view(request, id):
+    licenciatura = Licenciatura.get(id=id) 
+    return render(request, 'portfolio/licenciatura.html', {'licenciatura': licenciatura})
+
+def docentes_view(request):
+    docentes = Docente.objects.all()
+    return render(request, 'portfolio/docentes.html', {'docentes': docentes})
+
+def docente_view(request, id):
+    docente = Docente.objects.get(id=id)  
+    return render(request, 'portfolio/docente.html', {'docente': docente})
+
+def ucs_view(request):
+    ucs = UnidadeCurricular.objects.all()
+    return render(request, 'portfolio/ucs.html', {'ucs': ucs})
+
+def uc_view(request, id):
+    uc = UnidadeCurricular.objects.get(id=id)  
+    return render(request, 'portfolio/uc.html', {'uc': uc})
+
+def tecnologias_view(request):
+    tecnologias = Tecnologia.objects.all()
+    return render(request, 'portfolio/tecnologias.html', {'tecnologias': tecnologias})
+
+def tecnologia_view(request, id):
+    tecnologia = Tecnologia.objects.get(id=id)  
+    return render(request, 'portfolio/tecnologia.html', {'tecnologia': tecnologia})
+
+def competencias_view(request):
+    competencias = Competencia.objects.all()
+    return render(request, 'portfolio/competencias.html', {'competencias': competencias})
+
+def competencia_view(request, id):
+    competencia = Competencia.objects.get(id=id)
+    return render(request, 'portfolio/competencia.html', {'competencia': competencia})
+
+def tfcs_view(request):
+    tfcs = Tfc.objects.all()
+    return render(request, 'portfolio/tfcs.html', {'tfcs': tfcs})
+
+def tfc_view(request, id):
+    tfc = Tfc.objects.get(id=id)  
+    return render(request, 'portfolio/tfc.html', {'tfc': tfc})
+
+
+def projetos_view(request):
+    projetos = Projeto.objects.all()
+    return render(request, 'portfolio/projetos.html', {'projetos': projetos})
+
+def projeto_view(request, id):
+    projeto = Projeto.objects.get(id=id)  
+    return render(request, 'portfolio/projeto.html', {'projeto': projeto})
+
+def formacoes_view(request):
+    formacoes = Formacao.objects.all()
+    return render(request, 'portfolio/formacoes.html', {'formacoes': formacoes})
+
+def makingofs_view(request):
+    makingofs = MakingOf.objects.all()
+    return render(request, 'portfolio/makingofs.html', {'makingofs': makingofs})
+
+
+def eventos_view(request):
+    eventos = Evento.objects.all()
+    return render(request, 'portfolio/eventos.html', {'eventos': eventos})
+
+def evento_view(request, id):
+    evento = Evento.objects.get(id=id)  
+    return render(request, 'portfolio/evento.html', {'evento': evento})    
